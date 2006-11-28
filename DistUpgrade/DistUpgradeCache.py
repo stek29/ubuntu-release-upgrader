27741294d22f0b70f22b1ab85bf2578d203aee55
Michael Vogt <michael.vogt@ubuntu.com>
2006-11-28 17:02:10 +0100

    * DistUpgrade/DistUpgradeCache.py:   - feistyQuirks rule added: install ndiswrapper-utils-1.9 if 1.8 is installed

--- DistUpgrade/DistUpgradeCache.py
+++ DistUpgrade/DistUpgradeCache.py
@@ -163,6 +163,18 @@ class MyCache(apt.Cache):
         if func is not None:
             func()
 
+    def feistyQuirks(self):
+        """ this function works around quirks in the edgy->feisty upgrade """
+        logging.debug("running feistyQuirks handler")
+        for (fr, to) in [("ndiswrapper-utils-1.8","ndiswrapper-utils-1.9")]:
+            if self.has_key(fr) and self.has_key(to):
+                if self[fr].isInstalled and not self[to].markedInstall:
+                    try:
+                        self.markInstall(to,"%s->%s quirk upgrade rule" % (fr, to))
+                    except SystemError, e:
+                        logging.debug("Failed to apply %s->%s install (%s)" % (fr, to, e))
+        
+
     def edgyQuirks(self):
         """ this function works around quirks in the dapper->edgy upgrade """
         logging.debug("running edgyQuirks handler")

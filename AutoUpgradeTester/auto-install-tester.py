61592f47226e3a514fe3703124fdfd3fe617113a
Michael Vogt <michael.vogt@ubuntu.com>
2008-06-25 23:32:45 +0200

    add very simple auto-pkg-install-tester

---
+++ AutoUpgradeTester/auto-install-tester.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python
+
+import sys
+import os
+import os.path
+sys.path.insert(0, "../DistUpgrade")
+
+from UpgradeTestBackend import UpgradeTestBackend
+from UpgradeTestBackendQemu import *
+
+import apt
+
+if __name__ == "__main__":
+
+    # create backend
+
+    # FIXME: hardcoding pathes sucks
+    basedir = "./profile/intrepid-auto-install"
+    aptbasedir = os.path.join(basedir,"auto-install-test")
+    profile = os.path.join(basedir, "DistUpgrade.cfg")
+    backend = UpgradeTestBackendQemu(profile, profile)
+    backend.bootstrap()
+
+    # create dirs if needed
+    for d in ["etc/apt/",
+              "var/lib/dpkg",
+              "var/lib/apt/lists/partial",
+              "var/cache/apt/archives/partial"]:
+        if not os.path.exists(os.path.join(aptbasedir,d)):
+            os.makedirs(os.path.join(aptbasedir,d))
+    # write empty status file
+    open(os.path.join(aptbasedir,"var/lib/dpkg/","status"),"w")
+
+    # build apt stuff
+    mirror = backend.config.get("NonInteractive","Mirror")
+    dist = backend.config.get("Sources","From")
+    components = backend.config.getlist("NonInteractive","Components")
+    pockets =  backend.config.getlist("NonInteractive","Pockets")
+    f=open(os.path.join(aptbasedir,"etc","apt","sources.list"),"w")
+    f.write("deb %s %s %s\n" % (mirror, dist, " ".join(components)))
+    for pocket in pockets:
+        f.write("deb %s %s-%s %s\n" % (mirror, dist, pocket, " ".join(components)))
+    f.close()
+    
+    # get a cache
+    cache = apt.Cache(rootdir=aptbasedir) 
+    cache.update()
+    cache.open(apt.progress.OpProgress())
+
+
+    # now test if we can install stuff
+    backend.start()
+    backend._runInImage(["apt-get","update"])
+
+    # now see if we can install and remove it again
+    for pkg in cache:
+        ret = backend._runInImage(["DEBIAN_FRONTEND=noninteractive","apt-get","install", "-y",pkg.name])
+        assert(ret == 0)
+        ret = backend._runInImage(["DEBIAN_FRONTEND=noninteractive","apt-get","autoremove", "-y",pkg.name])
+    
+    # all done, stop the backend
+    backend.stop()
+

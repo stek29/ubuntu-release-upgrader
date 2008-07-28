b70cd2fb73918adddf54dec9a14ff686ab687423
Jonathan Riddell <jriddell@canonical.com>
2008-07-28 11:39:16 +0100

    add KDEFetchProgressAdapter and KDECdromProgressAdapter

--- DistUpgrade/DistUpgradeViewKDE4.py
+++ DistUpgrade/DistUpgradeViewKDE4.py
@@ -55,6 +55,26 @@ def utf8(str):
       return str
   return unicode(str, 'UTF-8')
 
+class KDECdromProgressAdapter(apt.progress.CdromProgress):
+    """ Report the cdrom add progress """
+    def __init__(self, parent):
+        self.status = parent.window_main.label_status
+        self.progressbar = parent.window_main.progressbar_cache
+        self.parent = parent
+
+    def update(self, text, step):
+        """ update is called regularly so that the gui can be redrawn """
+        if text:
+          self.status.setText(text)
+        self.progressbar.setValue(step/float(self.totalSteps))
+        KApplication.processEvents()
+
+    def askCdromName(self):
+        return (False, "")
+
+    def changeCdrom(self):
+        return False
+
 class KDEOpProgress(apt.progress.OpProgress):
   """ methods on the progress bar """
   def __init__(self, progressbar, progressbar_label):
@@ -74,6 +94,54 @@ class KDEOpProgress(apt.progress.OpProgress):
 
   def done(self):
       self.progressbar_label.setText("")
+
+class KDEFetchProgressAdapter(FetchProgress):
+    """ methods for updating the progress bar while fetching packages """
+    # FIXME: we really should have some sort of "we are at step"
+    # xy in the gui
+    # FIXME2: we need to thing about mediaCheck here too
+    def __init__(self, parent):
+        FetchProgress.__init__(self)
+        # if this is set to false the download will cancel
+        self.status = parent.window_main.label_status
+        self.progress = parent.window_main.progressbar_cache
+        self.parent = parent
+
+    def mediaChange(self, medium, drive):
+      msg = _("Please insert '%s' into the drive '%s'") % (medium,drive)
+      change = QMessageBox.question(self.parent.window_main, _("Media Change"), msg, QMessageBox.Ok, QMessageBox.Cancel)
+      if change == QMessageBox.Ok:
+        return True
+      return False
+
+    def start(self):
+        #self.progress.show()
+        self.progress.setValue(0)
+        self.status.show()
+
+    def stop(self):
+        self.parent.window_main.progress_text.setText("  ")
+        self.status.setText(_("Fetching is complete"))
+
+    def pulse(self):
+        """ we don't have a mainloop in this application, we just call processEvents here and elsewhere"""
+        # FIXME: move the status_str and progress_str into python-apt
+        # (python-apt need i18n first for this)
+        FetchProgress.pulse(self)
+        self.progress.setProgress(self.percent)
+        currentItem = self.currentItems + 1
+        if currentItem > self.totalItems:
+            currentItem = self.totalItems
+
+        if self.currentCPS > 0:
+            self.status.setText(_("Fetching file %li of %li at %sb/s") % (currentItem, self.totalItems, apt_pkg.SizeToStr(self.currentCPS)))
+            self.parent.window_main.progress_text.setText("<i>" + _("About %s remaining") % unicode(FuzzyTimeToStr(self.eta), 'utf-8') + "</i>")
+        else:
+            self.status.setText(_("Fetching file %li of %li") % (currentItem, self.totalItems))
+            self.parent.window_main.progress_text.setText("  ")
+
+        QApplication.processEvents()
+        return True
 
 # inherit from the class created in window_main.ui
 # to add the handler for closing the window
@@ -125,10 +193,10 @@ class DistUpgradeViewKDE4(DistUpgradeView):
         self.prev_step = 0 # keep a record of the latest step
 
         self._opCacheProgress = KDEOpProgress(self.window_main.progressbar_cache, self.window_main.progress_text)
-        """
         self._fetchProgress = KDEFetchProgressAdapter(self)
         self._cdromProgress = KDECdromProgressAdapter(self)
 
+        """
         self._installProgress = KDEInstallProgressAdapter(self)
 
         # reasonable fault handler

9112be722ba8e701baba21ef77fad574865f4897
Michael Vogt <michael.vogt@ubuntu.com>
2008-07-29 14:36:07 +0200

    add deprecation message to kde frontend

--- DistUpgrade/DistUpgradeViewKDE.py
+++ DistUpgrade/DistUpgradeViewKDE.py
@@ -18,6 +18,9 @@
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307
 #  USA
+#
+# THIS FRONTEND IS DEPRECATED IN FAVOR OF THE KDE4 ONE
+#
 
 from qt import *
 from kdeui import *

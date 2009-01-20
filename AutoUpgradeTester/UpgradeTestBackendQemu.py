6e96c280e1fa297bedfee81dfd61de750c20a350
Michael Vogt <michael.vogt@ubuntu.com>
2009-01-20 15:02:11 +0100

    AutoUpgradeTester/UpgradeTestBackendQemu.py: remove -tt option from ssh, causes hangs in intrepid->jaunty

--- AutoUpgradeTester/UpgradeTestBackendQemu.py
+++ AutoUpgradeTester/UpgradeTestBackendQemu.py
@@ -144,7 +144,7 @@ class UpgradeTestBackendQemu(UpgradeTestBackend):
         # ssh -l root -p 54321 localhost -i profile/server/ssh_key
         #     -o StrictHostKeyChecking=no
         ret = subprocess.call(["ssh",
-                               "-tt",
+#                               "-tt",
                                "-l","root",
                                "-p",self.ssh_port,
                                "localhost",

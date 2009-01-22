b8250a2e4ac2067e9d5ba103a6b596e565a2e984
Michael Vogt <michael.vogt@ubuntu.com>
2009-01-22 09:01:54 +0100

    * DistUpgrade/DevelReleaseAnnouncement:   - include a different release announcement for the     development releases * pre-build.sh:   - fix version parsing

--- pre-build.sh
+++ pre-build.sh
@@ -9,5 +9,6 @@
 #(cd tests && for test in *.py; do python $$test; done)
 
 # update version
-echo "VERSION='$(DEBVER)'" > DistUpgrade/DistUpgradeVersion.py
+DEBVER=$(LC_ALL=C dpkg-parsechangelog |sed -n -e '/^Version:/s/^Version: //p' | sed s/.*://)
+echo "VERSION='$DEBVER'" > DistUpgrade/DistUpgradeVersion.py
 

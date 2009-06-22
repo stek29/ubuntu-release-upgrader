07d353377833ac6272031fb2f2c03e37864df3fc
Michael Vogt <michael.vogt@ubuntu.com>
2009-06-22 10:20:32 +0200

    DistUpgrade/build-tarball.sh: fix in the build-tarball patch

--- DistUpgrade/build-tarball.sh
+++ DistUpgrade/build-tarball.sh
@@ -39,6 +39,6 @@ cp /usr/share/jockey/modaliases/nvidia-* modaliases
 cp /usr/share/jockey/modaliases/fglrx* modaliases
 
 # create the tarball, copy links in place 
-tar -c -h -z -v --exclude=$DIST.tar.gz --exclude=$0 -X build-exclude.txt -f $DIST.tar.gz  .
+tar -c -h -z -v --exclude=$DIST.tar.gz --exclude=$0 -X build-exclude.txt -f $DIST.tar.gz  ./*
 
 

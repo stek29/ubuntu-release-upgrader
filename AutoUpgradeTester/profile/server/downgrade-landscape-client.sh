003b489811fe80dec3acdd26bdcb1f66ba732e1c
Andreas Hasenack <andreas@canonical.com>
2009-06-26 13:25:34 -0300

    Include landscape-common in the downgrade

--- AutoUpgradeTester/profile/server/downgrade-landscape-client.sh
+++ AutoUpgradeTester/profile/server/downgrade-landscape-client.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-apt-get install -y --force-yes --allow-unauthenticated landscape-client=1.0.23-0ubuntu0.8.10.1
+apt-get install -y --force-yes --allow-unauthenticated landscape-common=1.0.23-0ubuntu0.8.10.1 landscape-client=1.0.23-0ubuntu0.8.10.1
 

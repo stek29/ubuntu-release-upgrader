32372a6da85e750d477d123bb30f1a31443acef8
Michael Vogt <michael.vogt@ubuntu.com>
2010-05-25 11:31:30 +0200

    * UpdateManager/backend/__init__.py:   - switch to aptdaemon as install backend by default

--- UpdateManager/backend/__init__.py
+++ UpdateManager/backend/__init__.py
@@ -4,13 +4,6 @@ import os.path
 def backend_factory(*args, **kwargs):
     " get a matching backend "
 
-    # try synaptic
-    if (os.path.exists("/usr/sbin/synaptic") and 
-        not "UPDATE_MANAGER_FORCE_BACKEND_APTDAEMON" in os.environ):
-        import InstallBackendSynaptic
-        return InstallBackendSynaptic.InstallBackendSynaptic(*args, **kwargs)
-
-
     # try the aptdaemon
     if os.path.exists("/usr/sbin/aptd"):
         # check if the gtkwidgets are installed as well
@@ -20,6 +13,12 @@ def backend_factory(*args, **kwargs):
             return InstallBackendAptdaemon.InstallBackendAptdaemon(*args, **kwargs)
         except ImportError, e:
             pass
+
+    # try synaptic
+    if (os.path.exists("/usr/sbin/synaptic") and 
+        not "UPDATE_MANAGER_FORCE_BACKEND_SYNAPTIC" in os.environ):
+        import InstallBackendSynaptic
+        return InstallBackendSynaptic.InstallBackendSynaptic(*args, **kwargs)
 
     # nothing found, raise
     raise Exception("No working backend found, please try installing synaptic or aptdaemon")

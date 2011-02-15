2e2ade693f5e0f11d812d12be94fdc59dc3feb72
Michael Vogt <michael.vogt@ubuntu.com>
2011-02-15 10:45:19 +0100

    add total count, add swig module special case, add keyring to the blacklist

--- AutoUpgradeTester/post_upgrade_tests/python_import_test.py
+++ AutoUpgradeTester/post_upgrade_tests/python_import_test.py
@@ -12,6 +12,10 @@ NEW_PYTHONVER="python2.7"
 OLD_BASEPATH="/usr/lib/%s/dist-packages/" % OLD_PYTHONVER
 NEW_BASEPATH="/usr/lib/%s/dist-packages/" % NEW_PYTHONVER
 
+# total imports
+TOTAL = 0
+FAIL = 0
+
 # stuff that we know does not work when doing a simple "import"
 blacklist = ["speechd_config", 
              "PAMmodule.so", 
@@ -21,6 +25,8 @@ blacklist = ["speechd_config",
              "desktopcouch",
              # just hangs
              "ropemacs",
+             # needs X
+             "keyring",
              ]
 
 def get_module_from_path(path):
@@ -28,10 +34,15 @@ def get_module_from_path(path):
         return f
     elif f.endswith(".py"):
         return f.split(".")[0]
+    # swig uses this, calls it "foomodule.so" but the import is "foo"
+    # (eg xdelta3module.so, pqueuemodule.so)
+    elif f.endswith("module.so"):
+        return f.split("modules.so")[0]
     elif f.endswith(".so"):
         return f.split(".")[0]
 
 def try_import(path):
+    global TOTAL, FAIL
     logging.info("Importing %s" % path)
     # a simple __import__(module) does not work, the problem
     # is that module import have funny side-effects (like
@@ -43,8 +54,10 @@ def try_import(path):
         return False
     cmd = ["python", "-c","import %s" % module]
     logging.debug("cmd: '%s'" % cmd)
+    TOTAL += 1
     ret = subprocess.call(cmd)
     if ret != 0:
+        FAIL += 1
         print "WARNING: failed to import '%s'" % module
         subprocess.call(["dpkg", "-S", os.path.realpath(path)])
         print "\n\n"
@@ -72,9 +85,13 @@ if __name__ == "__main__":
         ", ".join(old_modules - new_modules))
 
     res = True
+    # FIXME: instead os os.listdir() use os.walk() to catch subdirs
+    #        like lazr/* ?
     for f in filter(py_module_filter, os.listdir(NEW_BASEPATH)):
         logging.debug("looking at '%s'" % f)
         res &= try_import(os.path.join(NEW_BASEPATH, f))
-        
+
+    print "Total imports: %s" % TOTAL
+    print "Failures: %s" % FAIL
     if not res:
         sys.exit(1)

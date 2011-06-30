012a7442c275e8fc93cbc4858d412fec8d00b901
Brian Murray <brian@canonical.com>
2011-06-30 02:06:54 -0700

    remove default gconf values from update-manager apport hook as it is wasteful

--- debian/source_update-manager.py
+++ debian/source_update-manager.py
@@ -12,9 +12,6 @@ def add_info(report):
     # collect gconf settings for update-manager
     report['GconfUpdateManager'] = command_output(['gconftool-2', '-R',
         '/apps/update-manager'])
-    # grab the non-default values too as it is easier to compare with both the
-    # settings and the non-default values
-    attach_gconf(report, 'update-manager')
     attach_file_if_exists(report, '/var/log/apt/history.log',
         'DpkgHistoryLog.txt')
     attach_root_command_outputs(report,

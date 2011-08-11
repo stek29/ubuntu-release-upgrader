3fd49525271e9d0233a1862c82fccd253ef73206
Robert Roth <robert.roth.off@gmail.com>
2011-08-11 11:45:24 +0000

    Fixed changelog test with the new wording

--- tests/test_changelog.py
+++ tests/test_changelog.py
@@ -47,7 +47,7 @@ class TestChangelogs(unittest.TestCase):
         self.cache._get_changelog_or_news = monkey_patched_get_changelogs
         # get changelog
         self.cache.get_changelog(pkgname)
-	error = "This change is not coming from a source that supports changelogs."
+	error = "This update does not come from a source that supports changelogs."
         # verify that we don't have the lines twice
 	self.assertEqual(self.cache.all_changes[pkgname].split("\n")[-1], error)
 	self.assertEqual(len(self.cache.all_changes[pkgname].split("\n")), 5)

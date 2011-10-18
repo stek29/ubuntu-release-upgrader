90d147c84df3797533423e8f30e0d01d920f08e1
Michael Vogt <michael.vogt@ubuntu.com>
2011-10-18 16:21:06 +0200

    * DistUpgrade/DistUpgradeQuirks.py:   - keep poking the screensaver to ensure that it really won't     activate during the upgrade (thanks to Jonathan Davies for     the report)

--- DistUpgrade/DistUpgradeQuirks.py
+++ DistUpgrade/DistUpgradeQuirks.py
@@ -20,6 +20,7 @@
 #  USA
 
 import apt
+import atexit
 import glob
 import logging
 import os
@@ -430,6 +431,7 @@ class DistUpgradeQuirks(object):
         self._killUpdateNotifier()
         self._killKBluetooth()
         self._killScreensaver()
+        self._pokeScreensaver()
         self._stopDocvertConverter()
     def oneiricStartUpgrade(self):
         logging.debug("oneiric StartUpgrade quirks")
@@ -823,6 +825,19 @@ class DistUpgradeQuirks(object):
         if os.path.exists("/usr/bin/killall"):
             logging.debug("killing gnome-screensaver")
             subprocess.call(["killall", "-q", "gnome-screensaver"])
+    def _pokeScreensaver(self):
+        if os.path.exists("/usr/bin/xdg-screensaver"):
+            logging.debug("setup poke timer for the scrensaver")
+            self._poke = subprocess.Popen(
+                "while true; do /usr/bin/xdg-screensaver reset; sleep 30; done",
+                shell=True)
+            atexit.register(self._stopPokeScreensaver)
+    def _stopPokeScreensaver(self):
+        if self._poke:
+            self._poke.terminate()
+            res = self._poke.wait()
+            self._poke = None
+            return res
     def _removeBadMaintainerScripts(self):
         " remove bad/broken maintainer scripts (last resort) "
         # apache: workaround #95325 (edgy->feisty)

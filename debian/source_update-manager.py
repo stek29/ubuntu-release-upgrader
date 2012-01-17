b2a1b3de262c51ad626ed63299d380d2ebde9859
Brian Murray <brian@canonical.com>
2012-01-17 15:21:57 -0800

    debian/source_update-manager.py: include AptDaemon messages from syslog to help identify failures

--- debian/source_update-manager.py
+++ debian/source_update-manager.py
@@ -4,8 +4,10 @@
 Author: Brian Murray <brian@ubuntu.com>
 '''
 
+import re
 from apport.hookutils import (
-    attach_gsettings_package, attach_root_command_outputs, attach_file_if_exists)
+    attach_gsettings_package, attach_root_command_outputs,
+    attach_file_if_exists, recent_syslog)
 
 
 def add_info(report, ui):
@@ -24,9 +26,10 @@ def add_info(report, ui):
              'VarLogDistupgradeMainlog': 'cat /var/log/dist-upgrade/main.log',
              'VarLogDistupgradeSystemstatetargz': 'cat /var/log/dist-upgrade/system_state.tar.gz',
              'VarLogDistupgradeTermlog': 'cat /var/log/dist-upgrade/term.log'})
-    elif response is None:
+    elif response is None or response is False:
         attach_file_if_exists(report, '/var/log/apt/history.log',
             'DpkgHistoryLog.txt')
         attach_root_command_outputs(report,
             {'DpkgTerminalLog.txt': 'cat /var/log/apt/term.log',
              'CurrentDmesg.txt': 'dmesg | comm -13 --nocheck-order /var/log/dmesg -'})
+        report["Aptdaemon"] = recent_syslog(re.compile("AptDaemon"))

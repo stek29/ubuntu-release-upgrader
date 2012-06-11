13eb68f1a7bcd379c567589a8f2aa7c60965dc5a
Colin Watson <cjwatson@canonical.com>
2012-06-11 11:50:58 +0100

    Use apt_pkg.Acquire rather than apt_pkg.GetAcquire.

--- UpdateManager/GtkProgress.py
+++ UpdateManager/GtkProgress.py
@@ -202,7 +202,7 @@ if __name__ == "__main__":
     # generate a dist-upgrade (to feed data to the fetcher) and get it
     cache.upgrade()
     pm = apt_pkg.PackageManager(cache._depcache)
-    fetcher = apt_pkg.GetAcquire(acquire_progress)
+    fetcher = apt_pkg.Acquire(acquire_progress)
     res = cache._fetchArchives(fetcher, pm)
     print(res)
     

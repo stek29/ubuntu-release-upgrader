cf686fd85ea9e83acaf0a35e9cf1a78809dc82b0
Michael Terry <michael.terry@canonical.com>
2012-07-23 11:31:05 -0400

    * DistUpgrade/build-tarball.sh:   - Include .ui files in the tarball too, they got accidentally dropped     by me in the big code reshuffle (LP: #1026067)

--- DistUpgrade/build-tarball.sh
+++ DistUpgrade/build-tarball.sh
@@ -34,8 +34,8 @@ cp /usr/share/ubuntu-drivers-common/obsolete ubuntu-drivers-obsolete.pkgs
 # create the tarball, copy links in place 
 tar -c -h -v --exclude DistUpgrade --exclude=$DIST.tar --exclude=$0 -X build-exclude.txt -f $DIST.tar  ./*
 
-# add *.cfg to the tarball
-tar --append -v -f $DIST.tar --transform 's|.*/|./|' ../data/*.cfg
+# add *.cfg and *.ui to the tarball
+tar --append -v -f $DIST.tar --transform 's|.*/|./|' ../data/*.cfg ../data/gtkbuilder/*.ui
 
 # add "DistUpgrade"  symlink as symlink
 tar --append -v -f $DIST.tar ./DistUpgrade

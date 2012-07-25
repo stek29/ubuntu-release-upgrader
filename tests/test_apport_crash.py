238bb3d84e13a9bd4f6932fd0fd113d40d3a75a3
Brian Murray <brian@canonical.com>
2012-07-25 12:39:54 -0700

    add in test_apport_crash.py from bug 1004503

---
+++ tests/test_apport_crash.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import os
+import sys
+import tempfile
+import unittest
+
+from mock import patch
+
+sys.path.insert(0, "..")
+from DistUpgrade.DistUpgradeApport import (
+    _apport_append_logfiles,
+    apport_pkgfailure,
+    APPORT_WHITELIST,
+)
+
+class TestApportInformationLeak(unittest.TestCase):
+
+    def test_no_information_leak_in_apport_append_logfiles(self):
+        tmpdir = tempfile.mkdtemp()
+        report = {}
+        for name in ["apt.log", "system_state.tar.gz", "bar", "main.log"]:
+            with open(os.path.join(tmpdir, name), "w") as f:
+                f.write("some-data")
+        _apport_append_logfiles(report, tmpdir)
+        self.assertEqual(sorted([f[0].name for f in report.values()]),
+                         sorted([os.path.join(tmpdir, "main.log"),
+                                 os.path.join(tmpdir, "apt.log")]))
+        
+    @patch("subprocess.Popen")
+    def test_no_information_leak_in_apport_pkgfailure(self, mock_popen):
+        # call apport_pkgfailure with mocked data
+        apport_pkgfailure("apt", "random error msg")
+        # extract the call arguments
+        function_call_args, kwargs = mock_popen.call_args
+        apport_cmd_args = function_call_args[0]
+        # ensure that the whitelist is honored
+        for i in range(1, len(apport_cmd_args), 2):
+            option = apport_cmd_args[i]
+            arg = apport_cmd_args[i+1]
+            if option == "-l":
+                self.assertTrue(os.path.basename(arg) in APPORT_WHITELIST)
+
+
+if __name__ == "__main__":
+    unittest.main()

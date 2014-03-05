7a6126a08cd588e4aa1163a2786ac1a638ae5e3b
Brian Murray <brian@canonical.com>
2014-03-05 10:56:34 -0800

    DistUpgrade/DistUpgradeController.py: Catch exception if apport is not installed and fail gracefully. Thanks to Jean-Baptiste Lallement for the initial patch. (LP: #1285545)

--- DistUpgrade/DistUpgradeController.py
+++ DistUpgrade/DistUpgradeController.py
@@ -816,11 +816,20 @@ class DistUpgradeController(object):
             sourceslist.read_main_list()
         except SystemError:
             logging.error("Repository information invalid after updating (we broke it!)")
-            self._view.error(_("Repository information invalid"),
-                             _("Upgrading the repository information "
-                               "resulted in a invalid file so a bug "
-                               "reporting process is being started."))
-            subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+            if os.path.exists("/usr/bin/apport-bug"):
+                self._view.error(_("Repository information invalid"),
+                                 _("Upgrading the repository information "
+                                   "resulted in a invalid file so a bug "
+                                   "reporting process is being started."))
+                subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+            else:
+                self._view.error(_("Repository information invalid"),
+                                 _("Upgrading the repository information "
+                                   "resulted in a invalid file. To report "
+                                   "a bug install apport and then execute "
+                                   "'apport-bug ubuntu-release-upgrader'."))
+                logging.error("Missing apport-bug, bug report not "
+                              "autocreated")
             return False
 
         if self.sources_disabled:
@@ -1638,11 +1647,20 @@ class DistUpgradeController(object):
 
         if not self.prepare():
             logging.error("self.prepared() failed")
-            self._view.error(_("Preparing the upgrade failed"),
-                             _("Preparing the system for the upgrade "
-                               "failed so a bug reporting process is "
-                               "being started."))
-            subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+            if os.path.exists("/usr/bin/apport-bug"):
+                self._view.error(_("Preparing the upgrade failed"),
+                                 _("Preparing the system for the upgrade "
+                                   "failed so a bug reporting process is "
+                                   "being started."))
+                subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+            else:
+                self._view.error(_("Preparing the upgrade failed"),
+                                 _("Preparing the system for the upgrade "
+                                   "failed. To report a bug install apport "
+                                   "and then execute 'apport-bug "
+                                   "ubuntu-release-upgrader'."))
+                logging.error("Missing apport-bug, bug report not "
+                              "autocreated")
             sys.exit(1)
 
         # mvo: commented out for now, see #54234, this needs to be
@@ -1653,15 +1671,28 @@ class DistUpgradeController(object):
             logging.debug("need backports")
             # get backported packages (if needed)
             if not self.getRequiredBackports():
-                self._view.error(_("Getting upgrade prerequisites failed"),
-                                 _("The system was unable to get the "
-                                   "prerequisites for the upgrade. "
-                                   "The upgrade will abort now and restore "
-                                   "the original system state.\n"
-                                   "\n"
-                                   "Additionally, a bug reporting process is "
-                                   "being started."))
-                subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+                if os.path.exists("/usr/bin/apport-bug"):
+                    self._view.error(_("Getting upgrade prerequisites failed"),
+                                     _("The system was unable to get the "
+                                       "prerequisites for the upgrade. "
+                                       "The upgrade will abort now and restore "
+                                       "the original system state.\n"
+                                       "\n"
+                                       "Additionally, a bug reporting process is "
+                                       "being started."))
+                    subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+                else:
+                    self._view.error(_("Getting upgrade prerequisites failed"),
+                                     _("The system was unable to get the "
+                                       "prerequisites for the upgrade. "
+                                       "The upgrade will abort now and restore "
+                                       "the original system state.\n"
+                                       "\n"
+                                       "To report a bug install apport and "
+                                       "then execute 'apport-bug "
+                                       "ubuntu-release-upgrader'."))
+                    logging.error("Missing apport-bug, bug report not "
+                                  "autocreated")
                 self.abort()
 
         # run a "apt-get update" now, its ok to ignore errors, 
@@ -1735,7 +1766,11 @@ class DistUpgradeController(object):
                                    "In the case of an overloaded mirror, you "
                                    "may want to try the upgrade again later.")
                                    % pkg)
-                subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+                if os.path.exists("/usr/bin/apport-bug"):
+                    subprocess.Popen(["apport-bug", "ubuntu-release-upgrader-core"])
+                else:
+                    logging.error("Missing apport-bug, bug report not "
+                                  "autocreated")
                 self.abort()
 
         # calc the dist-upgrade and see if the removals are ok/expected

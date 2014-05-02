a1435cd5001d9c97cf6596d7d5ab3b9c2d8679ce
Michael Vogt <michael.vogt@ubuntu.com>
2014-05-02 13:29:38 +0200

    ./DistUpgrade/xorg_fix_proprietary.py: remove no longer needed code

--- DistUpgrade/xorg_fix_proprietary.py
+++ DistUpgrade/xorg_fix_proprietary.py
@@ -11,47 +11,10 @@ import os
 import logging
 import time
 import shutil
-import subprocess
-import apt_pkg
-apt_pkg.init()
 
 # main xorg.conf
 XORG_CONF="/etc/X11/xorg.conf"
 
-# really old and most likely obsolete conf
-OBSOLETE_XORG_CONF="/etc/X11/XF86Config-4"
-
-def remove_input_devices(xorg_source=XORG_CONF, xorg_destination=XORG_CONF):
-    logging.debug("remove_input_devices")
-    content=[]
-    in_input_devices = False
-    with open(xorg_source) as xorg_source_file:
-        for raw in xorg_source_file:
-            line = raw.strip()
-            if (line.lower().startswith("section") and 
-                line.lower().split("#")[0].strip().endswith('"inputdevice"')):
-                logging.debug("found 'InputDevice' section")
-                content.append("# commented out by ubuntu-release-upgrader, HAL is now used and auto-detects devices\n")
-                content.append("# Keyboard settings are now read from /etc/default/console-setup\n")
-                content.append("#"+raw)
-                in_input_devices=True
-            elif line.lower().startswith("endsection") and in_input_devices:
-                content.append("#"+raw)
-                in_input_devices=False
-            elif line.lower().startswith("inputdevice"):
-                logging.debug("commenting out '%s' " % line)
-                content.append("# commented out by ubuntu-release-upgrader, HAL is now used and auto-detects devices\n")
-                content.append("# Keyboard settings are now read from /etc/default/console-setup\n")
-                content.append("#"+raw)
-            elif in_input_devices:
-                logging.debug("commenting out '%s' " % line)
-                content.append("#"+raw)
-            else:
-                content.append(raw)
-    with open(xorg_destination+".new", "w") as xorg_destination_file:
-        xorg_destination_file.write("".join(content))
-    os.rename(xorg_destination+".new", xorg_destination)
-    return True
 
 def replace_driver_from_xorg(old_driver, new_driver, xorg=XORG_CONF):
     """
@@ -81,6 +44,7 @@ def replace_driver_from_xorg(old_driver, new_driver, xorg=XORG_CONF):
                 xorg_fix_file.write("".join(content))
             os.rename(xorg+".xorg_fix", xorg)
 
+
 def comment_out_driver_from_xorg(old_driver, xorg=XORG_CONF):
     """
     this comments out a driver from xorg.conf
@@ -108,15 +72,6 @@ def comment_out_driver_from_xorg(old_driver, xorg=XORG_CONF):
                 xorg_fix_file.write("".join(content))
             os.rename(xorg+".xorg_fix", xorg)
 
-def is_multiseat(xorg_source=XORG_CONF):
-    " check if we have a multiseat xorg config "
-    def is_serverlayout_line(line):
-        return (not line.strip().startswith("#") and
-                line.strip().lower().endswith('"serverlayout"'))
-    with open(xorg_source) as xorg_file:
-        msl = len([line for line in xorg_file if is_serverlayout_line(line)])
-    logging.debug("is_multiseat: lines %i", msl)
-    return msl > 1
 
 if __name__ == "__main__":
     if not os.getuid() == 0:
@@ -132,12 +87,6 @@ if __name__ == "__main__":
                         filemode='w')
     
     logging.info("%s running" % sys.argv[0])
-
-    if os.path.exists(OBSOLETE_XORG_CONF):
-        old = OBSOLETE_XORG_CONF
-        new =  OBSOLETE_XORG_CONF+".obsolete"
-        logging.info("renaming obsolete %s -> %s" % (old, new))
-        os.rename(old, new)
 
     if not os.path.exists(XORG_CONF):
         logging.info("No xorg.conf, exiting")
@@ -166,14 +115,3 @@ if __name__ == "__main__":
             if "nvidia" in xorg_conf_file.read():
                 logging.info("Removing nvidia from %s" % XORG_CONF)
                 comment_out_driver_from_xorg("nvidia")
-
-    # now run the removeInputDevices() if we have a new xserver
-    ver=subprocess.Popen(["dpkg-query","-W","-f=${Version}","xserver-xorg-core"],
-                         stdout=subprocess.PIPE,
-                         universal_newlines=True).communicate()[0]
-    logging.info("xserver-xorg-core version is '%s'" % ver)
-    if ver and apt_pkg.version_compare(ver, "2:1.5.0") > 0:
-        if not is_multiseat():
-            remove_input_devices()
-        else:
-            logging.info("multiseat setup, ignoring")

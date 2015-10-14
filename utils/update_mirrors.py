1d4e0edd1a210b087d2546d8f2e147de47169623
Brian Murray <brian@canonical.com>
2015-10-14 12:36:26 -0700

    utils/update_mirrors.py: when updating mirrors.cfg also check to see if an existing mirror is obsolete and remove it.

--- utils/update_mirrors.py
+++ utils/update_mirrors.py
@@ -4,20 +4,36 @@ import feedparser
 import sys
 
 # read what we have
-current_mirrors = set()
+current_mirrors = []
 with open(sys.argv[1], "r") as f:
     for line in f:
-        current_mirrors.add(line.strip())
+        current_mirrors.append(line.strip())
 
-    
 d = feedparser.parse("https://launchpad.net/ubuntu/+archivemirrors-rss")
 
 #import pprint
 #pp  = pprint.PrettyPrinter(indent=4)
 #pp.pprint(d)
 
-with open(sys.argv[1], "a") as outfile:
-    for entry in d.entries:
-        for link in entry.links:
-            if link.href not in current_mirrors:
-                outfile.write(link.href + "\n")
+lp_mirrors = set()
+new_mirrors = set()
+
+for entry in d.entries:
+    for link in entry.links:
+        lp_mirrors.add(link.href)
+        if link.href not in current_mirrors:
+            new_mirrors.add(link.href)
+
+with open(sys.argv[1], "w") as outfile:
+    # the first 23 lines are permanent
+    for mirror in current_mirrors[:22]:
+        outfile.write(mirror + "\n")
+    for mirror in current_mirrors[22:]:
+        if mirror.startswith("#"):
+            outfile.write(mirror + "\n")
+        # its not a valid mirror anymore
+        if mirror not in lp_mirrors:
+            continue
+        outfile.write(mirror + "\n")
+    for mirror in new_mirrors:
+        outfile.write(mirror + "\n")

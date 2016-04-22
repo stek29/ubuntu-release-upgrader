5802742b2d9d35815e86e9da8e01d7b40373d86a
Steve Langasek <steve.langasek@canonical.com>
2016-04-22 00:36:14 +0000

    And python3-update-manager missing from list of pre-build deps

--- pre-build.sh
+++ pre-build.sh
@@ -6,7 +6,7 @@ set -e
 export LANG='C.UTF-8'
 
 dpkg-checkbuilddeps -d 'python3-apt, apt-btrfs-snapshot, parsewiki, python3-feedparser,
-			python3-mock, xvfb, gir1.2-gtk-3.0, python3-gi, python3-nose, pep8, python3-distutils-extra'
+			python3-mock, xvfb, gir1.2-gtk-3.0, python3-gi, python3-nose, pep8, python3-distutils-extra, python3-update-manager'
 
 # update demotions
 # echo "Running demotions"

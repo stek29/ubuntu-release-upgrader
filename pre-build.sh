56fdcc4827a00e235b2f2efe9f4748988208e732
Brian Murray <brian@canonical.com>
2017-05-19 14:03:35 -0700

    Create demoted.cfg.xenial and DistUpgrade.cfg.xenial so that upgrades from Ubuntu 16.04 to Ubuntu 17.10 are supported. (LP: #1692092)

--- pre-build.sh
+++ pre-build.sh
@@ -13,7 +13,7 @@ dpkg-checkbuilddeps -d 'python3-apt, apt-btrfs-snapshot, parsewiki, python3-feed
 (cd utils && ./demotions.py zesty artful > demoted.cfg)
 # when this gets enabled, make sure to add symlink in DistUpgrade
 # echo "Running lts demotions"
-#(cd utils && ./demotions.py trusty xenial > demoted.cfg.trusty)
+(cd utils && ./demotions.py xenial artful > demoted.cfg.xenial)
 
 # update apt_btrfs_snapshot.py copy, this needs an installed
 # apt-btrfs-snapshot on the build machine

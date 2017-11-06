3cec3c560cabe6a8a3c0d40dc354395813f6f982
Brian Murray <brian@canonical.com>
2017-11-06 08:22:56 -0800

    * DistUpgrade/{EOL,Devel}ReleaseAnnouncement,   DistUpgrade/window_main.ui,   data/gtkbuilder/DistUpgrade.ui,   data/DistUpgrade.cfg,   data/DistUpgrade.cfg.xenial,   pre-build.sh:   - updated for 17.10 -> 18.04

--- pre-build.sh
+++ pre-build.sh
@@ -10,10 +10,10 @@ dpkg-checkbuilddeps -d 'python3-apt, apt-btrfs-snapshot, parsewiki, python3-feed
 
 # update demotions
 # echo "Running demotions"
-(cd utils && ./demotions.py zesty artful > demoted.cfg)
+(cd utils && ./demotions.py artful bionic > demoted.cfg)
 # when this gets enabled, make sure to add symlink in DistUpgrade
 # echo "Running lts demotions"
-(cd utils && ./demotions.py xenial artful > demoted.cfg.xenial)
+(cd utils && ./demotions.py xenial bionic > demoted.cfg.xenial)
 
 # update apt_btrfs_snapshot.py copy, this needs an installed
 # apt-btrfs-snapshot on the build machine

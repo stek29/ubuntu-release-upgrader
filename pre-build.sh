b69d7674c49730c997c474ad8ba82689ad0f37ef
Brian Murray <brian@canonical.com>
2019-04-25 08:49:02 -0700

    Fix broken symbolic link of DistUpgrade/DistUpgrade which caused code from the installed ubuntu-release-upgrader to be used instead of code in the dist-upgrader tarball. (LP: #1824430)

--- pre-build.sh
+++ pre-build.sh
@@ -8,6 +8,12 @@ export LANG='C.UTF-8'
 dpkg-checkbuilddeps -d 'python3-apt, apt-btrfs-snapshot, parsewiki, python3-feedparser,
 			python3-mock, xvfb, gir1.2-gtk-3.0, python3-gi, python3-nose, pep8, python3-distutils-extra, python3-update-manager'
 
+# make sure the DistUpgrade symlink is good LP: #1824430
+SYMLINK=$(readlink -q DistUpgrade/DistUpgrade)
+if [ $SYMLINK != "." ]; then
+   echo "DistUpgrade/DistUpgrade is not a symlink to itself"
+fi
+
 # run the test-suite
 # echo "Running integrated tests"
 xvfb-run nosetests3

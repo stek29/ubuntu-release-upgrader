4fb0ba06b481e2f1166dee61ee6e4699f07a5120
Brian Murray <brian@ubuntu.com>
2020-09-01 16:02:10 -0700

    releasing package ubuntu-release-upgrader version 1:20.10.11

--- utils/update_mirrors.py
+++ utils/update_mirrors.py
@@ -15,8 +15,8 @@ d = feedparser.parse("https://launchpad.net/ubuntu/+archivemirrors-rss")
 #pp  = pprint.PrettyPrinter(indent=4)
 #pp.pprint(d)
 
-# the first 24 lines are permanent
-permanent_lines = 24
+# the first 29 lines are permanent
+permanent_lines = 29
 lp_mirrors = set()
 new_mirrors = set()
 

45d488445b2c341d92c4fb64ccd4c69dc568a60f
Julian Andres Klode <julian.klode@canonical.com>
2021-07-05 10:47:37 +0200

    Allow upgrades on Apple systems again (LP: #1934650)
    Revert "DistUpgradeQuirks.py: only block upgrades for Apple hardware not all hardware that doesn't have DMI information. Thanks to Alyssa Rowan for the patch. (LP: #1931112)"
    
    This reverts commit a48c3f42f6fa926775238867889d368202b34bf8.
    
    Revert "DistUpgradeQuirks.py: If the system vendor is Apple do not allow release upgrades to 21.04 for the time being. (LP: #1929449)"
    
    This reverts commit 542d5e71644e25e7b60e74193cf408c74bd2d2ef.

--- DistUpgrade/DistUpgradeQuirks.py
+++ DistUpgrade/DistUpgradeQuirks.py
@@ -118,7 +118,6 @@ class DistUpgradeQuirks(object):
         self._get_from_and_to_version()
         self._test_and_fail_on_i386()
         self._test_and_fail_on_aufs()
-        self._test_and_fail_on_apple()
 
         cache = self.controller.cache
         self._test_and_warn_if_ros_installed(cache)
@@ -366,27 +365,6 @@ class DistUpgradeQuirks(object):
                         "%s and try again." % aufs_dir)
                 self._view.error(summary, msg)
                 self.controller.abort()
-
-    def _test_and_fail_on_apple(self):
-        """
-        Check to see if the system vendor is Apple and if so do not allow the
-        system to be upgraded. LP: #1928434
-        """
-        if not os.path.exists("/sys/firmware/efi"):
-            return
-        vendor = self._readDMIVendor()
-        if vendor.startswith('Apple Inc.') or \
-                vendor.startswith('Apple Computer, Inc.'):
-            logging.error("System vendor is Apple")
-        else:
-            return
-        summary = _("An upgrade is not possible at this time")
-        msg = _("Due to a bug in shim, LP: #1928434, upgrades are not "
-                "currently safe for your hardware.\n\n"
-                "Once that bug has been resolved you will be able to "
-                "upgrade to the next release of Ubuntu.")
-        self._view.error(summary, msg)
-        self.controller.abort()
 
     def _test_and_warn_if_vserver(self):
         """
